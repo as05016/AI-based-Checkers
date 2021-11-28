df2037d1ef5df733e7ec300b4909143e36dcc955
Altaf Shaikh <altaf_and18@hotmail.com>
2021-11-29 00:20:11 +0500

    Frontend Done

--- frontend/src/pages/test.js
+++ frontend/src/pages/test.js
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react'
 
 export const Page = ()=>{
 
-    useEffect(() =>{
-        fetch('http://127.0.0.1:5000/test').then(response =>{
-            if(response.ok){
-                return response.json()
-            }
-        }).then(data => console.log(data))
-    })
+    // useEffect(() =>{
+    //     fetch('http://127.0.0.1:5000/test').then(response =>{
+    //         if(response.ok){
+    //             return response.json()
+    //         }
+    //     }).then(data => console.log(data))
+    // })
 
     return(
         <>
